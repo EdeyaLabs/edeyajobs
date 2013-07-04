2c79d71eec21cbb51d250abd8c0b3e26f1ad5379
Vincent Paca <vincent.paca@gmail.com>
2013-07-04 10:03:39 +0800

    Added summary to jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,9 @@
 class Job < ActiveRecord::Base
   extend FriendlyId
 
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :summary
 
   friendly_id :title, use: :slugged
+
+  validates_presence_of :title, :description, :summary
 end
